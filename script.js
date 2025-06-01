d492b777b51acf5effdf17b227747e07d4e3fc7d
Murade Lobo <mubidalo@gmail.com>
2025-06-01 04:08:48 +0200

    Pop up

--- script.js
+++ script.js
@@ -26,3 +26,21 @@ function openmenu(){
 function closemenu(){
     sidemenu.style.right="-200px";
 }
+
+// Modal de Boas-vindas
+document.addEventListener('DOMContentLoaded', function() {
+    // Mostra o modal sempre que a página carregar
+    document.getElementById('welcomeModal').style.display = 'block';
+    
+    // Fecha o modal quando clicar no X
+    document.querySelector('.close').addEventListener('click', function() {
+        document.getElementById('welcomeModal').style.display = 'none';
+    });
+    
+    // Fecha o modal quando clicar fora da imagem
+    window.addEventListener('click', function(event) {
+        if (event.target == document.getElementById('welcomeModal')) {
+            document.getElementById('welcomeModal').style.display = 'none';
+        }
+    });
+});
